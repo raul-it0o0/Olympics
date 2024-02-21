0a0412512d4d6d29ef2e4f662e5d213017e125fe
raul-it0o0 <ariton.raul@gmail.com>
2024-02-21 14:06:25 +0200

    Initial commit

---
+++ Olympics.py
@@ -0,0 +1,10 @@
+# Specification:
+# dict = {"sport name":{"Country1":32, "Country2":78}]} OR
+# dict = {"sport name": [[Country1,Country2],[32,78]]}
+
+# 1. A function that receives a sport name and returns the top 3 countries (gold, silver, bronze medal receivers)
+# 2. A function that calculates the total points (sum) each country obtained -> tuple(["Country1","Country2"][66,23])
+# 3. A function that receives a tuple from function 2 and another tuple (last years results) and computes the percentage progress of each country.eval
+
+# Input comes from a csv file
+# Maybe try having a country as a class?
